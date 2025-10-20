5b20a0ce0c3bb26a09823e0566d3d4d0dcbde63e
PedroGj <pedrocsweb@gmail.com>
2025-10-20 12:36:35 -0300

    fix: add button logout in dashboard

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context';
+import { Box, Button, Typography } from '@mui/material';
+
 export const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/login');
+    } catch (err: any) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography
+        variant="h4"
+        gutterBottom
+      >
+        Bienvenido {user?.user_metadata?.name || user?.email}
+      </Typography>
+
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleLogout}
+      >
+        Cerrar sesión
+      </Button>
+    </Box>
+  );
 };
